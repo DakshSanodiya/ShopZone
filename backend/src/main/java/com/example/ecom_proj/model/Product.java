45f4f40413c4a5659ee13ad1951a5b9dc5a9169b
Daksh <dakshsanodiya@gmail.com>
2026-02-18 22:38:02 +0530

    Fixed compilation errors

--- backend/src/main/java/com/example/ecom_proj/model/Product.java
+++ backend/src/main/java/com/example/ecom_proj/model/Product.java
@@ -31,5 +31,13 @@ public class Product {
     @Lob
     private byte[] imageData;
 
+    public String getImageName() { return imageName; }
+    public void setImageName(String imageName) { this.imageName = imageName; }
+
+    public String getImageType() { return imageType; }
+    public void setImageType(String imageType) { this.imageType = imageType; }
+
+    public byte[] getImageData() { return imageData; }
+    public void setImageData(byte[] imageData) { this.imageData = imageData; }
 
 }
